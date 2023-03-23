ee1f759938beeb6b9d9610f4a0dfbceea23736f9
georgeyoumansjr <georgeyoumansjr@gmail.com>
2023-03-23 10:23:48 -0400

    Test Changes

--- colossus/settings.py
+++ colossus/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = config('SECRET_KEY', default=string.ascii_letters)
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', 
+                       cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.auth',
