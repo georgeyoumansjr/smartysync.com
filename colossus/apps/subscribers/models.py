14ef8d5b21ca0cd0d94c80a26b83524c2651d9fb
georgeyoumansjr <georgeyoumansjr@gmail.com>
2024-02-20 07:50:01 -0500

    automate unsubscribe removal

--- colossus/apps/subscribers/models.py
+++ colossus/apps/subscribers/models.py
@@ -222,6 +222,7 @@ class Subscriber(models.Model):
         with transaction.atomic():
             self.status = Status.UNSUBSCRIBED
             self.last_seen_ip_address = ip_address
+            # self.mailing_list = None
             self.save()
             self.create_activity(ActivityTypes.UNSUBSCRIBED, campaign=campaign, ip_address=ip_address)
 
@@ -514,8 +515,8 @@ class SubscriptionFormTemplate(models.Model):
         email.attach_alternative(rich_text_message, 'text/html')
         email.send()
 
-class Unsubscribers(models.Model):
-    email = email = models.EmailField(_('email address'), max_length=255)
+# class Unsubscribers(models.Model):
+#     email = email = models.EmailField(_('email address'), max_length=255)
 
-    def __str__(self):
-        return self.email
+#     def __str__(self):
+#         return self.email
--- remove_unsubscribed_ml.py
+++ remove_unsubscribed_ml.py
@@ -1,20 +1,17 @@
 import os 
 import django
+import logging
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "colossus.settings")
 django.setup()
 
 from datetime import datetime
 
-# Define imports and config dictionary
-import uuid
-import json
-import datetime
-import requests
+
 from dotenv import load_dotenv 
 from django.conf import settings
 
-import pytz
 
 from typing import Dict, List
 
@@ -30,70 +27,42 @@ from colossus.apps.lists.constants import ImportFields, ImportStatus
 from colossus.apps.lists.tasks import import_subscribers
 from colossus.apps.subscribers.constants import ActivityTypes, Status
 from colossus.apps.subscribers.fields import MultipleEmailField
-from colossus.apps.subscribers.models import Domain, Subscriber, Unsubscribers
+from colossus.apps.subscribers.models import Domain, Subscriber
 from colossus.apps.accounts.models import User
 
 from colossus.apps.lists.models import MailingList, SubscriberImport
 
-from colossus.apps.campaigns.models import Campaign
-from colossus.apps.campaigns.constants import CampaignStatus
+#create logs dir
+logs_directory = 'logs'
+if not os.path.exists(logs_directory):
+    os.makedirs(logs_directory)
 
-from email.header import decode_header
-import os
+# Configure logging
+logger = logging.getLogger("__name__")
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] - %(name)s - %(message)s")
+file_handler = logging.FileHandler(os.path.join("logs","remove_unsubs.log"))
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def get_subscribers(email):
     try:
-        subscribers = Subscriber.objects.filter(email=email,status=2)
+        subscribers = Subscriber.objects.filter(email=email)
         return subscribers
     except Subscriber.DoesNotExist:
-        print(f"Subscribers not found")
+        logger.error(f"Subscribers not found")
         return False
 
-
-def get_mailing_lists(userid):
-    try:
-        mailing_list = MailingList.objects.get(created_by_id=userid)
-        return mailing_list
-    except MailingList.DoesNotExist:
-        print(f'User does not exist, create the user with userid : {userid}')
-        return False
     
 
-def get_user(username):
-    try:
-        user = User.objects.get(username=username)
-        return user
-    except User.DoesNotExist:
-        print(f'User does not exist, create the user with username : {username}')
-        return False
-
-
-def get_users():
-    try:
-        users = User.objects.all().values('id', 'username', 'email')
-        return users
-    except User.DoesNotExist:
-        print(f'empty')
-        return False
-    
 
 def get_all_unsubscribed():
     try:
-        unsubscribers = Subscriber.objects.filter(status=3)
+        unsubscribers = Subscriber.objects.filter(status=3,mailing_list__isnull=False)
         return unsubscribers
     except User.DoesNotExist:
-        print(f'empty')
-        return False
-    
-def get_unsubscribers():
-    try:
-        unsubscribers = Unsubscribers.objects.all()
-        # Extract email addresses from Unsubscriber objects
-        email_addresses = [unsubscriber.email for unsubscriber in unsubscribers]
-        return email_addresses
-    except Unsubscribers.DoesNotExist:
-        print(f"Unsubscribers list empty")
+        logger.warning(f'Unsubscribers list is empty')
         return False
 
 
@@ -105,30 +74,36 @@ def remove_unsubscribed():
         "coboaccess3@gmail.com",
         "codeinspire1@gmail.com"
     ]
-    existing_unsubscribers = get_unsubscribers()
     for unsubscribed in unsubscribers:
-        if unsubscribed.email not in test_mails and unsubscribed.email not in existing_unsubscribers:
-            still_subscribers = get_subscribers(unsubscribed.email)
-            if still_subscribers:
-                for subscriber in still_subscribers:
-                    add_unsubscribers(subscriber.email)
-                    subscriber.delete()
-            else:
-                add_unsubscribers(unsubscribed.email)
+        if unsubscribed.email not in test_mails:
+            # add_unsubscribers(unsubscribed.email)
+            ml_id = unsubscribed.mailing_list
+            still_subscribed = get_subscribers(unsubscribed.email)
+            email = unsubscribed.email
+            logger.info(f"Processing for {email} from unsubscriber list")
+            if still_subscribed:
+                for subscribed in still_subscribed:
+                    try:
+                        subs_ml = subscribed.mailing_list
+                        subscribed.status = Status.UNSUBSCRIBED
+                        subscribed.save()
+                        subscribed.mailing_list = None
+                        subscribed.save()
+                        logger.info(f"Removed {email} from mailing list {str(subs_ml)}")
+                    except Exception as e:
+                        logger.exception(e)
+                        subscribed.delete()
+                        logger.warning(f"Deleted {email} due to exception")
+                    
+            try:
+                unsubscribed.mailing_list = None
+                unsubscribed.save()
+                logger.info(f"unsubscribed email {email} removed from mailing list {str(ml_id)}")
+            except Exception as e:
+                logger.exception(e)
                 unsubscribed.delete()
+                logger.warning(f"Deleted Subscribed {email} due to exception")
 
-
-def add_unsubscribers(email):
-    try:
-        unsubscriber, created = Unsubscribers.objects.get_or_create(email = email)
-
-        if unsubscriber:
-            print(f"Duplicate: The email {email} already exists in the Unsubscribers list")
-        if created:
-            print(f"Created: The email {email} is added to Unsubscribers list")
-
-    except Exception as e:
-        print(f"An error occurred: {str(e)} while adding unsubscribers")
 
 if __name__ == "__main__":
     remove_unsubscribed()
