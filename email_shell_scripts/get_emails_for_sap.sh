27251e62e75c66ce5404faea62523a5e04c142c2
Nausherwan Khalid Khan Niazi <53591215+knknausherwan@users.noreply.github.com>
2024-03-04 10:46:09 -0500

    removed unnecesary files and added check for sap scripts and facebook scripts are simplified

--- email_shell_scripts/get_emails_for_sap.sh
+++ email_shell_scripts/get_emails_for_sap.sh
@@ -7,5 +7,7 @@ source /home/coboit/thetitandev.com/venv/bin/activate
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] running the SAP script." | tee -a "$log_file"
 /home/coboit/thetitandev.com/venv/bin/python /home/coboit/thetitandev.com/get_emails_for_sap.py
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] done running the SAP script." | tee -a "$log_file"
+/home/coboit/thetitandev.com/venv/bin/python /home/coboit/thetitandev.com/check_if_sap_ran.py
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] done checking the SAP script." | tee -a "$log_file"
 
 deactivate
